db9c8cd7edd2f0549696ec54e4116d7b8ef15317
Hugo Rojas <hurojas.rios@gmail.com>
2019-07-04 22:24:29 +0000

    hola

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,22 +5,46 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			posts: []
+		};
+	}
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/posts")
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				this.setState({ posts: data });
+			});
+	}
+
 	render() {
-		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
-			</div>
-		);
+		console.log("render");
+		let peo = this.state.posts.map((item, i) => {
+			return (
+				<div key={i}>
+					<div className="card">
+						<img src="..." className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">{item.title}</h5>
+							<p className="card-text">
+								Some quick example text to build on the card
+								title and make up the bulk of the card's
+								content.
+							</p>
+							<a href="#" className="btn btn-primary">
+								{item.body}
+							</a>
+						</div>
+					</div>
+				</div>
+			);
+		});
+
+		return <div>{peo}</div>;
 	}
 }
